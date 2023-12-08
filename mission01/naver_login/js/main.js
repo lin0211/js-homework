501d53627462d942d65d52e454c483c6defb696d
lin0211 <slinj0211@gmail.com>
2023-12-08 17:22:08 +0900

    comment delete

--- mission01/naver_login/js/main.js
+++ mission01/naver_login/js/main.js
@@ -14,7 +14,6 @@ const inputEmail = document.querySelector(".user-email-input");
 const inputPW = document.querySelector(".user-password-input");
 const button = document.querySelector(".btn-login");
 
-// 1.email 정규표현식을 사용한 validation
 const emailValidation = () => {
   if (emailReg(inputEmail.value)) {
     inputEmail.classList.remove("is--invalid");
@@ -23,7 +22,6 @@ const emailValidation = () => {
   }
 };
 
-// 2. pw 정규표현식을 사용한 validation
 const pwValidation = () => {
   if (pwReg(inputPW.value)) {
     inputPW.classList.remove("is--invalid");
@@ -32,13 +30,10 @@ const pwValidation = () => {
   }
 };
 
-// 페이지 이동
 const linkToWelcome = () => {
   location.href = "welcome.html";
 };
 
-// 3. 로그인 버튼을 클릭시 user.pw의 값과 input의 값을 비교
-// 4. 두 값이 일치 한다면 다음 페이지(welcome.html)로 이동
 const handleLogIn = (e) => {
   e.preventDefault();
   if (inputEmail.value === user.id && inputPW.value === user.pw) {
@@ -63,6 +58,3 @@ function pwReg(text) {
   const re = /^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[!@#$%^*+=-]).{6,16}$/;
   return re.test(String(text).toLowerCase());
 }
-
-//제출
-//https://docs.google.com/forms/d/e/1FAIpQLSdkPbV4Fx0pjUHabjG3uWEsU7PkLe71o3oaQj5r-0KrjpZaCA/viewform
