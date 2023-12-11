17a8af6850e4206668594c97baff37913fd252a3
lin0211 <slinj0211@gmail.com>
2023-12-11 20:20:02 +0900

    상태변수의 관리

--- mission01/naver_login/js/main.js
+++ mission01/naver_login/js/main.js
@@ -14,6 +14,21 @@ const inputEmail = document.querySelector(".user-email-input");
 const inputPW = document.querySelector(".user-password-input");
 const button = document.querySelector(".btn-login");
 
+let emailPass = false;
+let pwPass = false;
+
+function emailReg(text) {
+  const re =
+    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+  return re.test(String(text).toLowerCase());
+}
+
+function pwReg(text) {
+  const re = /^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[!@#$%^*+=-]).{6,16}$/;
+  return re.test(String(text).toLowerCase());
+}
+
 const emailValidation = () => {
   if (emailReg(inputEmail.value)) {
     inputEmail.classList.remove("is--invalid");
@@ -36,25 +51,17 @@ const linkToWelcome = () => {
 
 const handleLogIn = (e) => {
   e.preventDefault();
-  if (inputEmail.value === user.id && inputPW.value === user.pw) {
-    linkToWelcome();
+  if (emailPass && pwPass) {
+    if (inputEmail.value === user.id && inputPW.value === user.pw) {
+      linkToWelcome();
+    } else {
+      inputEmail.value = "";
+      inputPW.value = "";
+      throw new Error("Not Matched");
+    }
   } else {
-    inputEmail.value = "";
-    inputPW.value = "";
-    throw new Error("Not Matched");
+    throw new Error("validation failed");
   }
 };
 
 button.addEventListener("click", handleLogIn);
-
-function emailReg(text) {
-  const re =
-    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-
-  return re.test(String(text).toLowerCase());
-}
-
-function pwReg(text) {
-  const re = /^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[!@#$%^*+=-]).{6,16}$/;
-  return re.test(String(text).toLowerCase());
-}
